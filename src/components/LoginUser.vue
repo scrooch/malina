cdd15dc597c4470e7aa23b0b53a7ea8c7f9d0ad0
scrooch <41049116+scrooch@users.noreply.github.com>
2022-03-31 09:10:03 +0200

    Remove selected row

--- src/components/LoginUser.vue
+++ src/components/LoginUser.vue
@@ -92,6 +92,20 @@
           </v-card-actions>
         </v-card>
       </v-dialog>
+      <v-dialog :value="!!current_row" persistent max-width="600px">
+        <v-card>
+          <v-card-title>
+            <span class="text-h5">{{ current_row }}</span>
+          </v-card-title>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="current_row = false">
+            Close
+          </v-btn>
+          <v-btn color="blue darken-1" text @click="delete_data">
+            Remove
+          </v-btn>
+        </v-card>
+      </v-dialog>
       <v-btn @click="get_data">Odświerz dane</v-btn>
       <v-snackbar v-model="snackbar" :timeout="timeout" color="pink accent-4">
         {{ snackbar_text }}
@@ -107,7 +121,7 @@ export default {
   data: () => ({
     timeout: 2000,
     snackbar: false,
-    snackbar_text: "Dane zostaly wyslane do bazy",
+    snackbar_text: "",
     loading: "",
     locations: "",
     headers: [
@@ -132,6 +146,7 @@ export default {
     errors: [],
     post_return: "",
     dialog: false,
+    current_row: "",
   }),
   async mounted() {
     this.loading = true;
@@ -147,7 +162,7 @@ export default {
   },
   methods: {
     handleClick(value) {
-      console.log(value.title);
+      this.current_row = value.title;
     },
     async get_data() {
       this.loading = true;
@@ -160,6 +175,26 @@ export default {
         })
         .then((response) => (this.locations = response));
       this.loading = false;
+    },
+    async delete_data() {
+      try {
+        await axios
+          .delete("https://exact-dove-16.hasura.app/api/rest/deletelocation", {
+            headers: {
+              "x-hasura-admin-secret": `${process.env.VUE_APP_HASURA_SECRET}`,
+            },
+            data: {
+              title: this.current_row,
+            },
+          })
+          .then((response) => (this.post_return = response));
+      } catch (e) {
+        this.errors.push(e);
+      }
+      this.current_row = false;
+      this.snackbar_text = "Dane zostaly usunięte z bazy";
+      this.snackbar = true;
+      await this.get_data();
     },
     async post_data() {
       try {
@@ -184,7 +219,9 @@ export default {
         this.errors.push(e);
       }
       this.dialog = false;
+      this.snackbar_text = "Dane zostaly wyslane do bazy";
       this.snackbar = true;
+      await this.get_data();
     },
   },
 };
